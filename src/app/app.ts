1fd355f98c6118474947d62d128e99e03872313a
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-07 19:50:58 -0300

    I create the register page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Login, Register],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
