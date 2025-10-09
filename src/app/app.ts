7c4479ca5dfea80aa178fed1d2fd3af6befd50c7
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-09 18:01:03 -0300

    Update to register page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Register } from './pages/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Register],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
