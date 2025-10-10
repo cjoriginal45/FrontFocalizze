cd2d86313d24f154a91912f7b71f14473859a835
cjoriginal45 <joaquinribarola45@gmail.com>
2025-10-10 12:55:03 -0300

    Login logic

---
+++ src/app/interfaces/LoginResponse.ts
@@ -0,0 +1,5 @@
+export interface LoginResponse {
+    userId: number;
+    token: string;
+    displayName: string;
+  }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,18 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../services/auth/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [MatIconModule, RouterLink],
+  imports: [  
+    CommonModule, 
+    FormsModule,  
+    MatIconModule,
+    RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnDestroy{
+
+  loginData = {
+    identifier: '',
+    password: ''
+  };
+
+  errorMessage: string | null = null;
+  private errorTimer: any = null; // Referencia al temporizador
+
   passwordInputType: string = 'password';
   showPasswordIcon: string = 'visibility';
 
-  constructor() {}
+  constructor(private authService: Auth, private router: Router) {}
+
+    // Método que se ejecuta al enviar el formulario.
+    onSubmit(): void {
+      this.errorMessage = null; 
+  
+      if (this.errorTimer) {
+        clearTimeout(this.errorTimer);
+      }
+  
+      this.authService.login(this.loginData).subscribe({
+        next: (response) => {
+          console.log('Login exitoso!', response);
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          console.error('Error en el login', err);
+          this.errorMessage = 'Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.';
+  
+          this.errorTimer = setTimeout(() => {
+            this.errorMessage = null;
+          }, 7000); // 7 segundos
+        }
+      });
+    }
+
 
   togglePasswordVisibility(): void {
     if (this.passwordInputType === 'password') {
@@ -21,6 +62,14 @@ export class Login {
     } else {
       this.passwordInputType = 'password';
       this.showPasswordIcon = 'visibility';
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    // Nos aseguramos de limpiar el temporizador para prevenir fugas de memoria
+    if (this.errorTimer) {
+      clearTimeout(this.errorTimer);
     }
   }
 }
