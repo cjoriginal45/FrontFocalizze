c1da7d6d73ec2802492ef54bd83035947a9abfa5
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-13 23:46:10 -0300

    Implement the test: Verify that the component is created correctly in register.spec.ts and implement the showError method in register.ts

--- src/app/pages/register/register.spec.ts
+++ src/app/pages/register/register.spec.ts
@@ -1,22 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
+import { ActivatedRoute } from '@angular/router';
 import { Register } from './register';
+import { RegisterService } from '../../services/registerService/register';
+import { Router } from '@angular/router';
+import { RegisterResponse } from '../../interfaces/RegisterResponse';
+import { of, throwError } from 'rxjs';
 
 describe('Register', () => {
   let component: Register;
   let fixture: ComponentFixture<Register>;
+  let registerService: jasmine.SpyObj<RegisterService>;
+  let router: jasmine.SpyObj<Router>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Register]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    const registerServiceSpy = jasmine.createSpyObj('RegisterService', ['register']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      imports: [Register],
+      providers: [
+        { provide: RegisterService, useValue: registerServiceSpy },
+        { provide: Router, useValue: routerSpy },
+
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { params: {} },
+          },
+        },
+      ],
+    });
 
     fixture = TestBed.createComponent(Register);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    registerService = TestBed.inject(RegisterService) as jasmine.SpyObj<RegisterService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
+  /**
+   * Prueba: Creación del componente
+   * Verifica que el componente se crea correctamente
+   *
+   * Test: Creating the component
+   * Verify that the component is created correctly
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
   });
