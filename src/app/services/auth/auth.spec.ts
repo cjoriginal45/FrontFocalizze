3c75b87e78cefd9a73649d9620ba0806868f0aa5
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-14 01:31:38 -0300

    Implement the test: Creating the service for the Auth.spec.ts service

--- src/app/services/auth/auth.spec.ts
+++ src/app/services/auth/auth.spec.ts
@@ -1,15 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { Auth } from './auth';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { LoginResponse } from '../../interfaces/LoginResponse';
+import { environment } from '../../environments/environment';
 
 describe('Auth', () => {
   let service: Auth;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [Auth, provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(Auth);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpMock.verify();
+    localStorage.clear();
+  });
+
+  /**
+   * Prueba: Creación del servicio
+   * Verifica que el servicio Auth se instancia correctamente
+   *
+   * Test: Creating the Service
+   * Verify that the Auth service is instantiated correctly
+   */
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
