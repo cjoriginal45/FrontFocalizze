f61e1e397c02f27d4261caa83d2f15fa03718b4f
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-14 22:39:02 -0300

    Create the menu service

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -4,8 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-menu',
   imports: [],
   templateUrl: './menu.html',
-  styleUrl: './menu.css'
+  styleUrl: './menu.css',
 })
-export class Menu {
-
-}
+export class Menu {}
