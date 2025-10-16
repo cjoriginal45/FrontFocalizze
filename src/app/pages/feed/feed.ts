bb2be2afcb513a2a20514404b5a1bb8366b8648c
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-15 22:27:35 -0300

    I save changes to feed.ts

--- src/app/pages/feed/feed.ts
+++ src/app/pages/feed/feed.ts
@@ -2,20 +2,11 @@ import { Component } from '@angular/core';
 import { Suggestions } from '../../components/suggestions/suggestions';
 import { BottonNav } from '../../components/botton-nav/botton-nav';
 import { Header } from '../../components/header/header';
-<<<<<<< HEAD
-import { Menu } from '../../components/menu/menu';
-
-@Component({
-  selector: 'app-feed',
-  imports: [Suggestions, BottonNav, Header, Menu],
-=======
 import { CreateThreadButton } from '../../components/create-thread-button/create-thread-button';
 
-
 @Component({
   selector: 'app-feed',
-  imports: [Suggestions, BottonNav, Header,CreateThreadButton],
->>>>>>> d05103b0a58ff2d62ae0ad4507849f0735aad686
+  imports: [Suggestions, BottonNav, Header, CreateThreadButton],
   templateUrl: './feed.html',
   styleUrl: './feed.css',
 })
