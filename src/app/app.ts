c7285338458b6453aa75edf1415d55e7cac52415
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-16 16:20:12 -0300

    I add the structure to the following-discover component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from './components/menu/menu';
+import { FollowingDiscovering } from './components/following-discovering/following-discovering';
+import { Feed } from './pages/feed/feed';
 
 @Component({
   selector: 'app-root',
