f60550bbb6674f21e42843b98bec37d43e5a3422
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-24 01:54:27 -0300

    Configure the route for the discover page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Suggestions } from './components/suggestions/suggestions';
 import { Feed } from './pages/feed/feed';
 import { Header } from './components/header/header';
 import { PasswordRecovery } from './pages/password-recovery/password-recovery';
+import { Discover } from './pages/discover/discover';
 
 export const routes: Routes = [
   { path: '', component: Login },
@@ -15,4 +16,5 @@ export const routes: Routes = [
   { path: 'suggestions', component: Suggestions },
   { path: 'home', component: Feed },
   { path: 'feed', component: Feed },
+  { path: 'discover', component: Discover },
 ];
