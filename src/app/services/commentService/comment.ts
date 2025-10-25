3e7c9671660f604a2a2e5048a48f6585ffec495a
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-24 23:23:05 -0300

    Implement the comment service logic

--- src/app/services/commentService/comment.ts
+++ src/app/services/commentService/comment.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Page } from '../../interfaces/PageInterface';
+import { CommentResponseDto } from '../../interfaces/CommentResponse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Comment {
-  
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiBaseUrl}/threads`;
+
+  getComments(threadId: number, page: number, size: number): Observable<Page<CommentResponseDto>> {
+    const params = new HttpParams().set('page', page).set('size', size);
+    return this.http.get<Page<CommentResponseDto>>(`${this.apiUrl}/${threadId}/comments`, {
+      params,
+    });
+  }
+
+  createComment(threadId: number, content: string): Observable<CommentResponseDto> {
+    return this.http.post<CommentResponseDto>(`${this.apiUrl}/${threadId}/comments`, { content });
+  }
 }
