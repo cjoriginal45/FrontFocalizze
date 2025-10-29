c1d8859edc3de4d75aa23ec0ae061294f4891694
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-29 00:58:39 -0300

    Add the logic for the saveService

--- src/app/services/saveService/save.ts
+++ src/app/services/saveService/save.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Save {
-  
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiBaseUrl}/v1/thread`; // Ruta base de hilos
+
+  /**
+   * Envía una petición para guardar o quitar el guardado de un hilo.
+   * @param threadId El ID del hilo afectado.
+   * @returns Un Observable que se completa cuando la operación finaliza.
+   *
+   * * Sends a request to save or unsave a thread.
+   * @param threadId The ID of the affected thread.
+   * @returns An Observable that completes when the operation completes.
+   */
+  toggleSave(threadId: number): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/${threadId}/save`, {});
+  }
 }
