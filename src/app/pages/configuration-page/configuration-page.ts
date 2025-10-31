81063482535ce1cd88b8a1d2b81370d9d359d74c
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-30 21:55:52 -0300

    Implement the configuration page structure

--- src/app/pages/configuration-page/configuration-page.ts
+++ src/app/pages/configuration-page/configuration-page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-configuration-page',
-  imports: [],
+  imports: [MatToolbar, MatIcon, MatNavList, Header],
   templateUrl: './configuration-page.html',
-  styleUrl: './configuration-page.css'
+  styleUrl: './configuration-page.css',
 })
-export class ConfigurationPage {
-
-}
+export class ConfigurationPage {}
