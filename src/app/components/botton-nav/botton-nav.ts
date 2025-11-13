df9a7ae5d1df87731dde4516cb474371b7641e33
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-11-13 00:47:45 -0300

    Modify the structure, design, and logic to add the avatar of the logged-in user

--- src/app/components/botton-nav/botton-nav.ts
+++ src/app/components/botton-nav/botton-nav.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Auth, AuthUser } from '../../services/auth/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-botton-nav',
-  imports: [  MatIconModule, RouterLink,RouterLinkActive ],
+  imports: [MatIconModule, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './botton-nav.html',
-  styleUrl: './botton-nav.css'
+  styleUrl: './botton-nav.css',
 })
 export class BottonNav {
-  constructor() {}
+  private authService = inject(Auth);
+  public currentUser: Signal<AuthUser | null>;
+
+  constructor() {
+    this.currentUser = this.authService.currentUser;
+  }
 }
