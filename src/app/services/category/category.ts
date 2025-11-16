c9527cd73fdf05adeaed8972bf76fb6ddfa10a59
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-11-16 17:52:13 -0300

    Modify the Category service

--- src/app/services/category/category.ts
+++ src/app/services/category/category.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CategoryInterface } from '../../interfaces/CategoryInterface';
+import { FeedThreadDto } from '../../interfaces/FeedThread';
+import { CategoryDetailsInterface } from '../../interfaces/CategoryDetailsInterface';
+import { Page } from '../../interfaces/PageInterface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Category {
-  private apiUrl = environment.apiBaseUrl+'/categories';
+  private apiUrl = environment.apiBaseUrl + '/categories';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllCategories(): Observable<CategoryInterface[]> {
     return this.http.get<CategoryInterface[]>(this.apiUrl);
+  }
+
+  getCategoryDetails(name: string): Observable<CategoryDetailsInterface> {
+    return this.http.get<CategoryDetailsInterface>(`${this.apiUrl}/${name}`);
+  }
+
+  getThreadsForCategory(name: string, page: number, size: number): Observable<Page<FeedThreadDto>> {
+    const params = new HttpParams().set('page', String(page)).set('size', String(size));
+    return this.http.get<Page<FeedThreadDto>>(`${this.apiUrl}/${name}/threads`, { params });
   }
 }
