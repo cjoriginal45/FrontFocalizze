694949801055315cd4d81ffc65e3c11ddfbb6ae0
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-11-22 14:04:51 -0300

    Create the discover-feed service

---
+++ src/app/services/discover-feed/discover-feed.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Page } from '../../interfaces/PageInterface';
+import { DiscoverItemDto } from '../../interfaces/DiscoverItemDto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DiscoverFeed {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiBaseUrl}/feed/discover`;
+
+  getDiscoverFeed(page: number, size: number): Observable<Page<DiscoverItemDto>> {
+    const params = new HttpParams().set('page', String(page)).set('size', String(size));
+
+    return this.http.get<Page<DiscoverItemDto>>(this.apiUrl, { params });
+  }
+}
