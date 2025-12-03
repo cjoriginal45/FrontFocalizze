432c9739d4a1947b35edac1259509677015f8fea
cjoriginal45 <joaquinribarola45@gmail.com>
2025-12-03 12:22:18 -0300

    report implementations

---
+++ src/app/services/report/report.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { ReportRequest } from '../../interfaces/ReportRequest';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Report {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiBaseUrl}/reports`;
+
+  reportUser(username: string, reportData: ReportRequest): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/users/${username}`, reportData);
+  }
+}
