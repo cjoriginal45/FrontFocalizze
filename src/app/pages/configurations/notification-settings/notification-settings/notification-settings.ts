ee9a13dd736b9cd41873a2b240df139c82de931c
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-12-20 17:06:27 -0300

    Refactoring of styles for the notification settings page

--- src/app/pages/configurations/notification-settings/notification-settings/notification-settings.ts
+++ src/app/pages/configurations/notification-settings/notification-settings/notification-settings.ts
@@ -1,12 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, Location as AngularLocation } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { Header } from "../../../../components/header/header";
-import { BottonNav } from "../../../../components/botton-nav/botton-nav";
+import { Header } from '../../../../components/header/header';
+import { BottonNav } from '../../../../components/botton-nav/botton-nav';
 
 interface NotificationSettingsInterface {
   newFollowers: boolean;
@@ -18,31 +18,40 @@ interface NotificationSettingsInterface {
 
 @Component({
   selector: 'app-notification-settings',
-  imports: [CommonModule,
+  imports: [
+    CommonModule,
     FormsModule,
     MatSlideToggleModule,
     MatCardModule,
     MatIconModule,
-    MatButtonModule, Header, BottonNav],
+    MatButtonModule,
+    Header,
+    BottonNav,
+  ],
   templateUrl: './notification-settings.html',
   styleUrl: './notification-settings.css',
 })
 export class NotificationSettings {
-// Estado inicial basado en la imagen (algunos on, algunos off)
-settings: NotificationSettingsInterface = {
-  newFollowers: false,
-  comments: true,
-  likes: false,
-  mentions: false,
-  dailySummary: true
-};
+  private location = inject(AngularLocation);
+  // Estado inicial basado en la imagen (algunos on, algunos off)
+  settings: NotificationSettingsInterface = {
+    newFollowers: false,
+    comments: true,
+    likes: false,
+    mentions: false,
+    dailySummary: true,
+  };
 
-constructor() {
-  // Aquí podrías cargar la configuración real del usuario
-}
+  constructor() {
+    // Aquí podrías cargar la configuración real del usuario
+  }
 
-// Método de ejemplo para guardar cambios
-saveSettings() {
-  console.log('Guardando configuración:', this.settings);
-}
+  // Método de ejemplo para guardar cambios
+  saveSettings() {
+    console.log('Guardando configuración:', this.settings);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
