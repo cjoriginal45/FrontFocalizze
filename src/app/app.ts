0993b66d8ad6c6318011a55b79925f88bde09e15
Joaquin Ribarola <157526664+cjoriginal45@users.noreply.github.com>
2026-02-08 14:28:01 -0300

    Enhance App class with OnInit and authService
    Implemented OnInit interface and added authService injection.

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,9 +8,17 @@ import { Theme } from './services/themeService/theme';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Focalizze');
 
   // Solo con inyectarlo, el constructor del servicio se ejecuta y aplica el tema
   private themeService = inject(Theme);
+
+  private authService = inject(Auth); // Inyectamos el servicio
+
+  ngOnInit() {
+    //eliminar
+    console.log('AppComponent inicializado, llamando a loadUserFromToken...');
+    this.authService.loadUserFromToken(); 
+  }
 }
